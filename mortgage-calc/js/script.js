f2bad392124d019f9795da8e3824ad8701ae6745
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2023-11-21 08:32:46 -0600

    Update

--- mortgage-calc/js/script.js
+++
@@ -1,6 +0,0 @@
-function calc() {
-   var Principal = 4
-   var p = document.getElementById("output");
-    var out = "";
-    out += "Principal:"       + Principal + "<br/>";
-}
---
+++ mortgage-calc/script.js
@@ -0,0 +1,4 @@
+var Principal = 4
+   var p = document.getElementById("output");
+    var out = "";
+    out += "Principal:"       + Principal + "<br/>";
