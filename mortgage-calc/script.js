d03bba409c29728e2d040ecec0cecdd5bdc50494
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2023-12-01 08:01:40 -0600

    Update

---
+++ mortgage-calc/script.js
@@ -0,0 +1,14 @@
+function doMath() {
+    var p = document.getElementById('p').value;
+    p = Number(p);
+    var r = document.getElementById('r').value;
+    r = Number(r);
+    var t = document.getElementById('t').value;
+    t = Number(t);
+    var m = document.getElementById('m').value;
+    m = Number(m);
+    var n = document.getElementById('n').value;
+    n = Number(n);
+
+    var B = x
+}
