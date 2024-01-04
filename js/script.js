de5ceb49b45f8cc3d7ea86d26fa75af131d11352
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-01-04 08:46:04 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -6,33 +6,10 @@ const updateCursorPosition = (event) => {
 }
 window.addEventListener('mousemove', (event) => {
   updateCursorPosition(event)
-  
+
   if (hoverContainer.matches(':hover')) {
     customCursor.classList.add('zoom')
   } else {
     customCursor.classList.remove('zoom')
   }
 })
-
-var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
-
-var x = setInterval(function() {
-
-  var now = new Date().getTime();
-
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (10000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (10000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (10000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (10000 * 60)) / 1000);
-
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
-  }
-}, 1000);
