49bb87917c93c4538f4f3b14f35bd7b697f3421f
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-01-04 08:29:48 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -22,15 +22,15 @@ var x = setInterval(function() {
 
   var distance = countDownDate - now;
 
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  var days = Math.floor(distance / (10000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (10000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (10000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (10000 * 60)) / 1000);
 
   document.getElementById("demo").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
-  // If the count down is finished, write some text
+
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
