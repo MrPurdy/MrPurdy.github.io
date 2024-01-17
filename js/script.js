6c8926d04a8ea3fc7e6507148eaedbdb05a4581c
MrPurdy <143815144+MrPurdy@users.noreply.github.com>
2024-01-17 08:12:51 -0600

    Update

--- js/script.js
+++ js/script.js
@@ -15,42 +15,46 @@ window.addEventListener('mousemove', (event) => {
 })
 
 
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function openLightbox() {
+  document.getElementById('Lightbox').style.display = 'block';
 }
 
-
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
+function closeLightbox() {
+  document.getElementById('Lightbox').style.display = 'none';
+};
 
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+function changeSlide(n) {
+  showSlide(slideIndex += n);
+};
 
+function toSlide(n) {
+  showSlide(slideIndex = n);
+};
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+function showSlide(n) {
+  const slides = document.getElementsByClassName('slide');
+  let modalPreviews = document.getElementsByClassName('modal-preview');
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  };
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  };
+
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
+  };
+
+  for (let i = 0; i < modalPreviews.length; i++) {
+    modalPreviews[i].className = modalPreviews[i].className.replace(' active', '');
+  };
+
+  slides[slideIndex - 1].style.display = 'block';
+  modalPreviews[slideIndex - 1].className += ' active';
+};
